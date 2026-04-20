07be4c1858485afe542b506ab54773b664d02189
H6LS1S <seagull.bogdan@gmail.com>
2026-04-20 21:38:22 +0300

    fix(app): theme flicker and ignore device theme

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { Sun, ChevronsLeft, Play, Pause } from 'lucide-svelte/icons';
 	import { Router } from 'sv-router';
-	import { onMount } from 'svelte';
 
 	import { navigate, route } from 'sv-router/generated';
 	import { Button } from '@/lib/button';
@@ -15,11 +14,6 @@
 		document.documentElement.classList.toggle('dark', theme);
 		localStorage.setItem('theme', theme ? 'dark' : 'light');
 	};
-
-	onMount(() => {
-		document.documentElement.classList.toggle('dark', theme);
-		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeTheme);
-	});
 </script>
 
 <header class="m-6 flex items-center justify-between">
